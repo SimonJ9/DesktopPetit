2b6b4ff04c50768e1571a333a5d51ed81192f93d
SimonJ-PC\SimonJ9 <simonj142857@gmail.com>
2018-02-15 04:41:34 -0500

    Let Camera ignore the background

---
+++ src/Assets/Materials/TransparentWindow.shader
@@ -0,0 +1,74 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Custom/TransparentWindow" 
+{
+	Properties
+	{
+		_MainTex("Albedo (RGB)", 2D) = "white" {}
+		_TransparentColorKey("Transparent Color Key", Color) = (0, 0, 0, 1)
+		_TransparencyTolerance("Transparency Tolerance", Float) = 0.01
+	}
+	SubShader
+	{
+		Pass 
+		{
+			Tags { "RenderType" = "Opaque" }
+			LOD 200
+
+			CGPROGRAM
+
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+
+			//a? to vertex
+			struct a2v
+			{
+				float4 pos : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			//vertex to fragment
+			struct v2f
+			{
+				float4 pos : SV_POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			//vertex shader function
+			v2f vert(a2v input)
+			{
+				v2f output;
+				//transform points from object space to camera's clip space in homogeneous coordinates
+				output.pos = UnityObjectToClipPos(input.pos);
+				output.uv = input.uv;
+
+				return output;
+			}
+
+			sampler2D _MainTex;
+			float3 _TransparentColorKey;
+			float _TransparencyTolerance;
+
+			//frag shader function
+			float4 frag(v2f input) : SV_Target
+			{
+				float4 color = tex2D(_MainTex, input.uv);
+
+				float deltaR = abs(color.r - _TransparentColorKey.r);
+				float deltaG = abs(color.g - _TransparentColorKey.g);
+				float deltaB = abs(color.b - _TransparentColorKey.b);
+
+				if (deltaR < _TransparencyTolerance &&
+					deltaG < _TransparencyTolerance &&
+					deltaB < _TransparencyTolerance)
+				{
+					return float4(0.0f, 0.0f, 0.0f, 0.0f);
+				}
+
+				return color;
+			}
+			ENDCG
+		}
+	}
+}
---
+++ src/Assets/Scripts/TransparentWindow.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Runtime.InteropServices;   //for importing windows native api
+using UnityEngine;
+
+public class TransparentWindow : MonoBehaviour
+{
+    [SerializeField]
+    private Material m_Material;
+
+    private struct MARGINS
+    {
+        public int csLeftWidth;
+        public int csRightWidth;
+        public int cyTopHeight;
+        public int cyBottomHeight;
+    }
+
+    //not sure
+    //Windows Native API ??????
+    //need to be defined ????
+
+
+    //"retieves the window handle to the active window attached to the calling thread's message queue
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetActiveWindow();
+
+    //"Change an attribute of the specified window"
+    [DllImport("user32.dll")]
+    private static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNetLong);
+
+    //"Extends the window frame into the client area"
+    [DllImport("Dwmapi.dll")]
+    private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS margins);
+
+    //??????
+    //Definitions of window styles
+    const int GWL_STYLE = -16;
+    const uint WS_POPUP = 0x80000000;
+    const uint WS_VISIBLE = 0x10000000;
+    
+
+    private void Start()
+    {
+        #if !UNITY_EDITOR
+        var margins = new MARGINS() { csLeftWidth = -1};
+
+        //handle
+        var hwnd = GetActiveWindow();
+
+        //set properties
+        SetWindowLong(hwnd, GWL_STYLE, WS_POPUP | WS_VISIBLE);
+
+        //Extend window into client area
+        DwmExtendFrameIntoClientArea(hwnd, ref margins);
+
+        #endif
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, m_Material);
+    }
+
+}
